1c0688239e26ad9565a47e6f1b4343424ae71e18
redarashid <redarashid824@gmail.com>
2024-10-10 23:33:50 +0300

    first commit

--- src/Components/Contact/MapContact.js
+++ src/Components/Contact/MapContact.js
@@ -32,21 +32,25 @@ const MapContact = () => {
     </div>
 
     <div className='contact'>
+
        <div className='item'>
           <i class="fa-solid fa-envelope"></i>
           <h4>Email Address</h4>
           <a href="mailto:example@example.com">info@company.com</a>
        </div>
+
        <div className='item'>
           <i class="fa-solid fa-phone"></i>
           <h4>Phone Number</h4>
           <a href="mailto:example@example.com">010-020-0340</a>
        </div>
+
        <div className='item'>
           <i class="fa-solid fa-map-location-dot"></i>
           <h4>Address</h4>
-          <a href="mailto:example@example.com">Victoria, Australia</a>
+          <a href="mailto:example@example.com">Victoria,Australia</a>
        </div>
+
     </div>
        </Container>
     </div>
